be3d8f7c50261126958e12f0a7e7c286c4e3b168
theInternauts <cricketw@wearetheinternauts.com>
2018-04-18 02:17:33 -0700

    add credits to main page and enable 3D scroller on sheet-view

--- src/app/sheet-view/sheet-view.component.ts
+++ src/app/sheet-view/sheet-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input }   from '@angular/core';
+import { Component, Input }   from '@angular/core';
 import { ResortService }  from '../resort.service';
 
 type AOA = any[][];
@@ -8,19 +8,39 @@ type AOA = any[][];
   templateUrl: './sheet-view.component.html',
   styleUrls: ['./sheet-view.component.css']
 })
-export class SheetViewComponent implements OnInit {
+export class SheetViewComponent {
   @Input() data: AOA;
   @Input() activeSection: string;
 
-  constructor(private resortService: ResortService) { }
+  has3Dintitialized: boolean;
 
-  ngOnInit(): void {}
+  constructor(private resortService: ResortService) {
+    this.has3Dintitialized = false;
+  }
+
+  ngAfterViewChecked() {
+    if(!this.has3Dintitialized) {
+      this.start3DScroller();
+      this.has3Dintitialized = true;
+    }
+  }
 
   ngOnChanges(): void {
+    this.has3Dintitialized = false;
     this.data = this.resortService.getData();
   }
 
   isActiveSection(sectionName: string): boolean {
     return (this.activeSection == sectionName);
+  }
+
+  start3DScroller(): void {
+    // Call the foldscroll plugin
+    // TODO: submit pull request to public repo for update to plugin --> add an unmout/disable function to avoid memory leaks
+    $( '.sheet-view.container-3d-panel-block' ).foldscroll({
+      perspective: 900,
+      margin: '220px'
+    });
+    console.log("SheetViewComponent: start3DScroller");
   }
 }

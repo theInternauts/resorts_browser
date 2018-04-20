4ea35d1878f77a6b04af54a342da075b442d8410
theInternauts <cricketw@wearetheinternauts.com>
2018-04-20 02:20:30 -0700

    make message service panel a lot more presentable and usefule for debugging/notifications

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -3,8 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MessageService {
   messages: string[] = [];
+  showMessagePanel: boolean;
 
-  constructor() { }
+  constructor() {
+    this.showMessagePanel = true;
+  }
 
   add(message: string) {
     this.messages.push(message);
@@ -12,5 +15,14 @@ export class MessageService {
 
   clear() {
     this.messages = [];
+  }
+  messagePanelIsVisble(): boolean {
+    return this.showMessagePanel;
+  }
+  closeMessagePanel(): boolean {
+    return this.showMessagePanel = false;
+  }
+  openMessagePanel(): boolean {
+    return this.showMessagePanel = true;
   }
 }
